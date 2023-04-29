ff433f7292ab7ed95d2282cff544d4355943b7f6
shafayetullah7 <shafayet769@gmail.com>
2023-04-29 18:20:16 +0600

    env-r1

--- src/firebase/firebase-init.js
+++ src/firebase/firebase-init.js
@@ -4,7 +4,6 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// console.log('env : ',import.meta.env.VITE_PASS);
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_APIKEY,
   authDomain: import.meta.env.VITE_AUTHDOMAIN,
@@ -13,12 +12,5 @@ const firebaseConfig = {
   messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
   appId: import.meta.env.VITE_APPID
 };
-// VITE_PASS=SECRET
-// VITE_APIKEY=AIzaSyDD5-jkBbopHl470n3vWO7pSqFDPu32hRs
-// VITE_AUTHDOMAIN=practice-2-93b86.firebaseapp.com
-// VITE_PROJECTID=practice-2-93b86
-// VITE_STORAGEBUCKET=practice-2-93b86.appspot.com
-// VITE_MESSAGINGSENDERID=370992960175
-// VITE_APPID=1:370992960175:web:d93f38d52b59b41a5d3a5a
-// Initialize Firebase
+
 export const app = initializeApp(firebaseConfig);
